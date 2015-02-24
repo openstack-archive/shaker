7163707ce3180b6f88a99e5fc744196ef46fe422
Ilya Shakhat <ishakhat@mirantis.com>
2015-02-24 19:28:37 +0300

    Added iperf executor with chart feature
    Change-Id: I5dd6c56c615c86a4fc3400a951d9739205cff604

--- shaker/engine/report.py
+++ shaker/engine/report.py
@@ -14,30 +14,15 @@
 # limitations under the License.
 
 import sys
-import uuid
 
 import jinja2
 
 from shaker.engine import utils
 
 
-def _add_uuids(d):
-    for k, v in d.items():
-        if isinstance(v, dict):
-            _add_uuids(v)
-        elif isinstance(v, list):
-            for i in v:
-                if isinstance(i, dict):
-                    _add_uuids(i)
-        else:
-            d['uuid'] = uuid.uuid4()
-
-
 def generate_report(report_template, report_filename, data):
     template = utils.read_file(report_template)
     compiled_template = jinja2.Template(template)
-
-    _add_uuids(data)
 
     rendered_template = compiled_template.render(dict(report=data))
 
