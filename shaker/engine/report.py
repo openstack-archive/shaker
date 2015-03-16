d34743d1b65f84961055f60552804cee189c137d
Ilya Shakhat <shakhat@gmail.com>
2015-03-17 00:17:32 +0300

    Implement SLA support and subunit output
    Change-Id: I6ae7a18ce9aff45af574d1f0ab656de251df5ea4

--- shaker/engine/report.py
+++ shaker/engine/report.py
@@ -20,6 +20,7 @@ import sys
 import jinja2
 from oslo_config import cfg
 from oslo_log import log as logging
+from subunit import v2 as subunit_v2
 import yaml
 
 from shaker.engine import aggregators
@@ -43,11 +44,68 @@ def calculate_stats(data):
         aggregator.test_summary(test_result)
 
 
-def generate_report(report_template, report_filename, data):
+def verify_sla(data, subunit_filename):
+    if not subunit_filename:
+        return
+
+    fd = open(subunit_filename, 'w')
+    output = subunit_v2.StreamResultToBytes(fd)
+
+    for test_result in data['result']:
+        sla = test_result['definition'].get('sla')
+        if not sla:
+            continue
+
+        test_name = (test_result['definition'].get('title') or
+                     test_result['definition'].get('class'))
+        for iteration_result in test_result['results_per_iteration']:
+            for agent_result in iteration_result['results_per_agent']:
+                stats = agent_result['stats']
+
+                for k, params in sla.items():
+                    if k not in stats:
+                        LOG.warning('SLA parameter %s not found', k)
+                        continue
+                    for p, value in params.items():
+                        if p not in stats[k]:
+                            LOG.warning('SLA parameter %s:%s not found', k, p)
+                            continue
+
+                        sla_name = '%s.%s.%s.%s.%s' % (
+                            test_name,
+                            len(iteration_result['results_per_agent']),
+                            agent_result['agent']['id'],
+                            k, p)
+                        output.startTestRun()
+                        status = value < stats[k][p]
+
+                        if value < stats[k][p]:
+                            LOG.debug('SLA %s OK', sla_name)
+                        else:
+                            LOG.debug('SLA %s FAILED', sla_name)
+
+                        output.status(
+                            test_id=sla_name,
+                            file_name='results',
+                            mime_type='text/plain; charset="utf8"',
+                            eof=True,
+                            file_bytes=yaml.safe_dump(
+                                stats[k], default_flow_style=False))
+
+                        output.status(
+                            test_id=sla_name,
+                            test_status='success' if status else 'fail')
+                        output.stopTestRun()
+
+    fd.close()
+
+
+def generate_report(data, report_template, report_filename, subunit_filename):
     LOG.debug('Generating report, template: %s, output: %s',
               report_template, report_filename or 'stdout')
 
     calculate_stats(data)
+    verify_sla(data, subunit_filename)
 
     # add more filters to jinja
     jinja_env = jinja2.Environment()
@@ -75,7 +133,8 @@ def main():
     LOG.debug('Reading JSON data from: %s', cfg.CONF.input)
     report_data = json.loads(utils.read_file(cfg.CONF.input))
 
-    generate_report(cfg.CONF.report_template, cfg.CONF.report, report_data)
+    generate_report(report_data, cfg.CONF.report_template, cfg.CONF.report,
+                    cfg.CONF.subunit)
 
 
 if __name__ == "__main__":
