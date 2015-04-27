301a5941e2f42417e6149cbc908207de8222e872
Ilya Shakhat <ishakhat@mirantis.com>
2015-04-27 19:57:21 +0300

    Add separate setup config for building pyshaker-agent
    Change-Id: Iebc8804c662e2457f079e3bd9fe13106e6200450

---
+++ tools/build_agent_pkg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TEMP_DIR="$(mktemp -d)"
+cp -r shaker ${TEMP_DIR}/
+cp requirements-agent.txt ${TEMP_DIR}/requirements.txt
+cp setup-agent.cfg ${TEMP_DIR}/setup.cfg
+cp setup.py ${TEMP_DIR}/
+cp README.rst ${TEMP_DIR}/
+cp LICENSE ${TEMP_DIR}/
+
+export PBR_VERSION="$(git describe --exact-match)"
+
+cd ${TEMP_DIR}
+echo "Building pyshaker-agent version ${PBR_VERSION}"
+python setup.py sdist upload
+
+cd -
+rm -rf ${TEMP_DIR}
