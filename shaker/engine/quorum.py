c3da1fe7caf692efd5c2a80293bb131dfaaf2994
Ilya Shakhat <shakhat@gmail.com>
2016-06-14 19:57:25 +0300

    Asynchronous scenario execution
    With this patch Shaker is able to run scenario in asynchronous manner.
    There is no need to have agent inside of instances and instances call
    back via HTTP. This allows to use Cirros-based instances with small
    user scripts.
    
    One of use cases for this execution mode is verification of connectivity
    and metadata retrieval. The sample scenario deploys instance per compute
    node, each instances retrieves its id from metadata service and reports
    it back to Shaker. Upon success the report should contain replies from
    all instances.
    
    Change-Id: Icc86379a8157efbd0228704c0fe36f462a9642fb

--- shaker/engine/quorum.py
+++ shaker/engine/quorum.py
@@ -13,7 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import cgi
+import functools
 import multiprocessing
+import six
 import time
 import traceback
 
@@ -22,6 +25,7 @@ from oslo_log import log as logging
 from shaker.agent import agent as agent_process
 from shaker.engine.executors import base as base_executors
 from shaker.engine import messaging
+from shaker.engine import utils
 
 
 LOG = logging.getLogger(__name__)
@@ -133,7 +137,12 @@ class CleanOperation(BaseOperation):
         return reply
 
 
-class Quorum(object):
+class BaseQuorum(object):
+    def execute(self, executors):
+        pass
+
+
+class Quorum(BaseQuorum):
     def __init__(self, message_queue, polling_interval, agent_loss_timeout,
                  agent_join_timeout):
         self.message_queue = message_queue
@@ -220,7 +229,7 @@ class Quorum(object):
         return self._run(ExecuteOperation(executors))
 
 
-class LocalQuorum(object):
+class LocalQuorum(BaseQuorum):
     def execute(self, executors):
         operation = ExecuteOperation(executors)
         agent_ids = operation.get_active_agent_ids()
@@ -230,6 +239,88 @@ class LocalQuorum(object):
             task = operation.get_reply(agent_id, None)
             command_res = agent_process.run_command(task.get('command'))
             result[agent_id] = operation.process_reply(agent_id, command_res)
+
+        return result
+
+
+class HTTPPostHandler(six.moves.BaseHTTPServer.BaseHTTPRequestHandler):
+
+    def __init__(self, request, client_address, server, result_queue):
+        self.result_queue = result_queue
+        six.moves.BaseHTTPServer.BaseHTTPRequestHandler.__init__(
+            self, request, client_address, server)
+
+    def do_POST(self):
+        # Parse the form data posted
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST',
+                     'CONTENT_TYPE': self.headers['Content-Type'],
+                     })
+
+        # Send the response
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write("Hello!\n")
+
+        result = dict((k, form[k].value) for k in form)
+
+        if 'agent_id' in result:
+            self.result_queue.put(result)
+
+
+def run_http_server(host, port, result_queue):
+    handler = functools.partial(HTTPPostHandler, result_queue=result_queue)
+    server = six.moves.BaseHTTPServer.HTTPServer((host, port), handler)
+    server.serve_forever()
+
+
+class AsyncQuorum(BaseQuorum):
+    def __init__(self, server_endpoint, agent_loss_timeout):
+        self.server_endpoint = server_endpoint
+        self.agent_join_timeout = agent_loss_timeout
+
+    def execute(self, executors):
+        result_queue = multiprocessing.Queue()
+
+        host, port = utils.split_address(self.server_endpoint)
+
+        worker = multiprocessing.Process(
+            target=run_http_server,
+            kwargs=dict(host=host, port=int(port), result_queue=result_queue))
+        worker.daemon = True
+        worker.start()
+
+        LOG.info('HTTP listener is started at %s:%s', host, port)
+
+        operation = ExecuteOperation(executors)
+        agent_ids = operation.get_active_agent_ids()
+        deadline = time.time() + self.agent_join_timeout
+        result = {}
+
+        while (time.time() < deadline) and len(result.keys()) < len(agent_ids):
+            try:
+                item = result_queue.get(timeout=10)
+                agent_id = item['agent_id']
+
+                if agent_id not in agent_ids:
+                    continue  # ignore unexpected
+
+                result[agent_id] = operation.process_reply(agent_id, item)
+
+                LOG.debug('Received: %s', item)
+
+                if len(result.keys()) == len(agent_ids):
+                    break
+            except six.moves.queue.Empty:
+                pass
+
+        lost = agent_ids - set(result.keys())
+        if lost:
+            LOG.warning('Lost agents: %s', lost)
+            result.update(dict((a_id, operation.process_failure(a_id))
+                          for a_id in lost))
 
         return result
 
@@ -259,3 +350,7 @@ def make_quorum(agent_ids, server_endpoint, polling_interval,
 
 def make_local_quorum():
     return LocalQuorum()
+
+
+def make_async_quorum(server_endpoint, agent_join_timeout):
+    return AsyncQuorum(server_endpoint, agent_join_timeout)
