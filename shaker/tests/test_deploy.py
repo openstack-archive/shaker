011bbc516ec344c158a6ce75081abfe98cff8aaf
Oded Le'Sage <ol7435@att.com>
2018-10-31 09:08:24 -0500

    Enhance Shaker to provide the ability to define support templates
    Issue: At AT&T we have large complex test stacks that make putting
    everything into a single heat template and environment file very
    cumbersome. Large monolithic templates make it harder to debug failures,
    maintain, extend, and organize these tests. In order to solve this issue
    we have enhanced Shaker to support specifying support templates with
    environment files.
    
    This commit enhances Shaker to add the ability to define
    support_templates with env_files in test definitions.
    
    Support templates spin up "support type" resources before the actual
    test stack is spun up. This could range from networks, to volumes, to
    anything Heat can create. The support resources do not have any reliance
    on resources created in the test stack, they set up a "foundation" for
    the test stack. The test stack can then reference these resources by
    name. (e.g. assume they exist by the time the test stack is spun up)
    
    While the example provided with this commit is simple, and the support
    networks that get created are not directly used in the test, it
    shows the basic principles of how support templates work.
    
    As a real-world example and to give an idea of the complexity this
    enhancement is trying to solve, we have a test definition that looks
    like this:
    
      support_templates:
      -
        Base:
          template: templates/module_1_base.yaml
          env_file: env/module_1_base.env
      -
       SI_L2:
          template: templates/module_2_si_l2.yaml
          env_file: env/module_2_si_l2.env
      -
       SI_L3:
          template: templates/module_3_si_l3.yaml
          env_file: env/module_3_si_l3.env
    
      template: templates/module_4_master_servant.yaml
      env_file: env/module_4_master_servant.env
    
    The first support stack (module_1) sets up some "base" network
    resources. This stack provides some network resources used by the SI_L2
    and SI_L3 support stacks.
    
    SI_L2 is a support stack with 2 VMs that do Contrail service chaining
    on an L2 network.
    
    SI_L3 is a support stack with 2 VMs that do Contrail service chaining
    on an L3 network.
    
    Then the test stack (module 4) gets spun up on N amount of computes and
    runs traffic across the SI_L2 and SI_L3 service chained networks.
    
    After the test run all stacks are cleaned up
    
    Using the concept of support stacks allows us to beter organize and
    maintain our complex tests and allows for faster debugging due to the
    "layered" nature of the setup.
    
    Support templates also allow us to spin up more Shaker test threads
    that use the same support templates simultaneously to better simulate
    real-world network traffic. It also reduces the set up time of certain
    tests we have since the support stacks already exist.
    
    This enhancement does not alter existing Shaker functionality and
    is fully backwards compatible.
    
    Change-Id: Ife51bc55874c6ec4faac221bab8f9f0eea175fdc

--- shaker/tests/test_deploy.py
+++ shaker/tests/test_deploy.py
@@ -20,14 +20,17 @@ import mock
 import os
 import re
 import testtools
+import uuid
 
 from oslo_config import cfg
 from oslo_config import fixture as config_fixture_pkg
 from shaker.engine import config
 from shaker.engine import deploy
 from shaker.engine import utils
+from shaker.openstack.clients import heat
 from shaker.openstack.clients import nova
 from shaker.tests import fakes
+from timeout_decorator import TimeoutError
 
 ZONE = 'zone'
 
@@ -576,13 +579,13 @@ class TestDeploy(testtools.TestCase):
         absolute_path = utils.resolve_relative_path(test_file)
         scenario = utils.read_yaml_file(absolute_path)
 
-        heat_id = 'shaker_abcdefg'
+        stack_name = 'shaker_abcdefg'
 
         server_endpoint = "127.0.0.01"
         base_dir = os.path.dirname(absolute_path)
 
         deployment = deploy.Deployment()
-        deployment.stack_name = heat_id
+        deployment.stack_name = stack_name
         deployment.external_net = 'test-external_net'
         deployment.image_name = 'test-image'
         deployment.flavor_name = 'test-flavor'
@@ -597,13 +600,13 @@ class TestDeploy(testtools.TestCase):
 
         nova_nodes_mock.return_value = [{'host': 'host-1', 'zone': 'nova'}]
 
-        create_stack_mock.create_stack.return_value = heat_id
+        create_stack_mock.return_value = uuid.uuid4()
 
         heat_outputs = {
-            heat_id + '_master_0_instance_name': 'instance-0000052f',
-            heat_id + '_master_0_ip': '192.0.0.3',
-            heat_id + '_slave_0_ip': '192.0.0.4',
-            heat_id + '_slave_0_instance_name': 'instance-0000052c'}
+            stack_name + '_master_0_instance_name': 'instance-0000052f',
+            stack_name + '_master_0_ip': '192.0.0.3',
+            stack_name + '_slave_0_ip': '192.0.0.4',
+            stack_name + '_slave_0_instance_name': 'instance-0000052c'}
 
         stack_output_mock.return_value = heat_outputs
 
@@ -628,6 +631,139 @@ class TestDeploy(testtools.TestCase):
                                              base_dir=base_dir)
 
         self.assertEqual(expected, agents)
+
+    @mock.patch('shaker.openstack.clients.heat.get_stack_outputs')
+    @mock.patch('shaker.openstack.clients.heat.create_stack')
+    @mock.patch('shaker.openstack.clients.openstack.OpenStackClient')
+    @mock.patch('shaker.engine.deploy.Deployment._get_compute_nodes')
+    def test_deploy_from_hot_with_support_stacks(self, nova_nodes_mock,
+                                                 openstack_mock,
+                                                 create_stack_mock,
+                                                 stack_output_mock):
+        test_file = 'shaker/scenarios/test/sample_with_support_stacks.yaml'
+        absolute_path = utils.resolve_relative_path(test_file)
+        scenario = utils.read_yaml_file(absolute_path)
+
+        stack_name = 'shaker_abcdefg'
+
+        server_endpoint = "127.0.0.01"
+        base_dir = os.path.dirname(absolute_path)
+
+        deployment = deploy.Deployment()
+        deployment.stack_name = stack_name
+        deployment.external_net = 'test-external_net'
+        deployment.image_name = 'test-image'
+        deployment.flavor_name = 'test-flavor'
+        deployment.dns_nameservers = '8.8.8.8'
+        deployment.openstack_client = openstack_mock
+
+        nova_nodes_mock.return_value = [{'host': 'host-1', 'zone': 'nova'}]
+
+        create_stack_mock.return_value = uuid.uuid4()
+
+        heat_outputs = {
+            stack_name + '_master_0_instance_name': 'instance-0000052f',
+            stack_name + '_master_0_ip': '10.0.0.3',
+            stack_name + '_slave_0_ip': '10.0.0.4',
+            stack_name + '_slave_0_instance_name': 'instance-0000052c'}
+
+        stack_output_mock.return_value = heat_outputs
+
+        expected = {
+            'shaker_abcdefg_master_0': {'availability_zone': 'nova:host-1',
+                                        'id': 'shaker_abcdefg_master_0',
+                                        'ip': '10.0.0.3',
+                                        'mode': 'master',
+                                        'node': 'host-1',
+                                        'slave_id': 'shaker_abcdefg_slave_0',
+                                        'zone': 'nova'},
+            'shaker_abcdefg_slave_0': {'availability_zone': 'nova:host-1',
+                                       'id': 'shaker_abcdefg_slave_0',
+                                       'ip': '10.0.0.4',
+                                       'master_id': 'shaker_abcdefg_master_0',
+                                       'mode': 'slave',
+                                       'node': 'host-1',
+                                       'zone': 'nova'}}
+
+        agents = deployment._deploy_from_hot(scenario['deployment'],
+                                             server_endpoint,
+                                             base_dir=base_dir)
+
+        self.assertEqual(create_stack_mock.call_count, 3)
+        self.assertEqual(expected, agents)
+
+    @mock.patch('shaker.openstack.clients.heat.create_stack')
+    @mock.patch('shaker.openstack.clients.openstack.OpenStackClient')
+    def test_deploy_support_stacks(self, openstack_mock, create_stack_mock):
+        test_file = 'shaker/scenarios/test/sample_with_support_stacks.yaml'
+        absolute_path = utils.resolve_relative_path(test_file)
+        scenario = utils.read_yaml_file(absolute_path)
+
+        support_stacks = scenario['deployment']['support_templates']
+        base_dir = os.path.dirname(absolute_path)
+
+        deployment = deploy.Deployment()
+        deployment.openstack_client = openstack_mock
+
+        support_stack_1 = uuid.uuid4()
+        support_stack_2 = uuid.uuid4()
+
+        create_stack_mock.side_effect = (support_stack_1, support_stack_2)
+
+        deployment._deploy_support_stacks(support_stacks, base_dir)
+
+        self.assertEqual(support_stack_1, deployment.support_stacks[0].id)
+        self.assertEqual(support_stack_2, deployment.support_stacks[1].id)
+
+    @mock.patch('shaker.openstack.clients.heat.create_stack')
+    @mock.patch('shaker.openstack.clients.openstack.OpenStackClient')
+    def test_deploy_support_stacks_with_conflict(self, openstack_mock,
+                                                 create_stack_mock):
+        test_file = 'shaker/scenarios/test/sample_with_support_stacks.yaml'
+        absolute_path = utils.resolve_relative_path(test_file)
+        scenario = utils.read_yaml_file(absolute_path)
+
+        support_stacks = scenario['deployment']['support_templates']
+        base_dir = os.path.dirname(absolute_path)
+
+        deployment = deploy.Deployment()
+        deployment.openstack_client = openstack_mock
+
+        support_stack_1 = heat.exc.Conflict
+        support_stack_2 = uuid.uuid4()
+
+        create_stack_mock.side_effect = (support_stack_1, support_stack_2)
+
+        deployment._deploy_support_stacks(support_stacks, base_dir)
+
+        self.assertEqual(support_stack_2, deployment.support_stacks[0].id)
+        self.assertEqual(create_stack_mock.call_count, 2)
+
+    @mock.patch('shaker.openstack.clients.heat.get_stack_status')
+    @mock.patch('shaker.openstack.clients.openstack.OpenStackClient')
+    def test_wait_stack_deletion(self, openstack_mock, get_status_mock):
+        get_status_mock.side_effect = heat.exc.HTTPNotFound
+        stack_id = uuid.uuid4()
+
+        self.assertIsNone(heat.wait_stack_deletion(openstack_mock, stack_id))
+
+    @mock.patch('shaker.openstack.clients.heat.get_stack_status')
+    @mock.patch('shaker.openstack.clients.openstack.OpenStackClient')
+    def test_wait_stack_deletion_failed_stack(self, openstack_mock,
+                                              get_status_mock):
+        get_status_mock.return_value = ('FAILED', 'some_reason')
+        stack_id = uuid.uuid4()
+        self.assertRaises(heat.exc.StackFailure, heat.wait_stack_deletion,
+                          openstack_mock, stack_id)
+
+    @mock.patch('shaker.openstack.clients.heat.get_stack_status')
+    @mock.patch('shaker.openstack.clients.openstack.OpenStackClient')
+    def test_wait_stack_deletion_timeout(self, openstack_mock,
+                                         get_status_mock):
+        get_status_mock.side_effect = TimeoutError
+        stack_id = uuid.uuid4()
+
+        self.assertIsNone(heat.wait_stack_deletion(openstack_mock, stack_id))
 
     @mock.patch('shaker.openstack.clients.openstack.OpenStackClient')
     def test_get_compute_nodes_flavor_no_extra_specs(self,
